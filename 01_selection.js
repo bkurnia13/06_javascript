78fa66d0c8903b026750936e748915033bef969d
bkurnia13 <bayu.kdp@gmail.com>
2020-01-19 12:56:52 +0800

    Create 01_selection .html and .js files

---
+++ 01_selection.js
@@ -0,0 +1,37 @@
+// // document.getElementById -> element
+// const judul = document.getElementById('judul');
+// judul.style.color = 'red';
+// judul.style.backgroundColor = '#ccc';
+
+
+
+// // document.getElementsByTagName -> HTMLCollections
+// const p = document.getElementsByTagName('p');
+// for ( let i = 0; i < p.length; i++ ) {
+//     p[i].style.backgroundColor = 'lightblue';
+// }
+
+// const h1 = document.getElementsByTagName('h1')[0];
+// h1.style.fontSize = '50px';
+
+
+
+// // document.getElementsByClassName -> HTMLCollections
+// const p1 = document.getElementsByClassName('p1');
+// p1[0].innerHTML = 'Ini dirubah dari javascript';
+
+
+
+// document.querySelector -> element
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+
+
+// document.querySelectorAll -> nodeList
+const p = document.querySelectorAll('p');
+p[2].style.backgroundColor = 'lightblue';
