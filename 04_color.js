6048c5427f38869546cfe33fce850c46f44a92cc
bkurnia13 <bayu.kdp@gmail.com>
2020-01-22 00:05:41 +0800

    Create 04_color .html and .js files

---
+++ 04_color.js
@@ -0,0 +1,63 @@
+// Tombol Ubah Warna
+const tUbahWarna = document.getElementById('tUbahWarna');
+tUbahWarna.addEventListener('click', function() {
+    // document.body.style.backgroundColor = 'lightblue';
+    // document.body.setAttribute('class', 'biru-muda');
+    document.body.classList.toggle('biru-muda');
+});
+
+
+
+// Tombol Acak Warna
+const tAcakWarna = document.createElement('button');
+const teksTombol = document.createTextNode('Acak Warna');
+tAcakWarna.appendChild(teksTombol);
+tAcakWarna.setAttribute('type', 'button');
+tUbahWarna.after(tAcakWarna);
+
+tAcakWarna.addEventListener('click', function() {
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+
+
+// Slider
+const sMerah = document.querySelector('input[name=sMerah]');
+const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
+
+sMerah.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sHijau.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sBiru.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+
+
+// Ubah Warna Berdasarkan Posisi Cursor Mouse
+document.body.addEventListener('mousemove', function(event) {
+    // Cari Posisi Cusrsor = event.clientX;
+    // Cari Ukuran Halaman = window.innerWidth;
+
+    const xPos = (event.clientX / window.innerWidth) * 255;
+    const yPos = (event.clientY / window.innerHeight) * 255;
+    document.body.style.backgroundColor = 'rgb('+ xPos +','+ yPos +',100)';
+});
